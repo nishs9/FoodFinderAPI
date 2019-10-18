f8e003d87679f679b7a1380fe09ed50245834e42
Nishant Sethunath <snishk@gmail.com>
2019-10-18 14:03:05 -0400

    Added endpoint for uploading images to the database

--- models/Image.js
+++ models/Image.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const autoIncrement = require('mongoose-auto-increment');
+var connection = mongoose.createConnection("mongodb://nishs9:mas2019@mas-cluster-shard-00-00-whhsb.mongodb.net:27017,mas-cluster-shard-00-01-whhsb.mongodb.net:27017,mas-cluster-shard-00-02-whhsb.mongodb.net:27017/mas-database?ssl=true&replicaSet=MAS-Cluster-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+autoIncrement.initialize(connection);
 
 const ImageSchema = new Schema({
+  imgID: {
+    type: Number,
+    required: true
+  },
   img: {
       data: Buffer,
   },
@@ -12,3 +20,10 @@ const ImageSchema = new Schema({
 });
 
 module.exports = mongoose.model("Image", ImageSchema);
+
+ImageSchema.plugin(autoIncrement.plugin, {
+  model: 'Image',
+  field: 'imgID',
+  startAt: '1000',
+  incrementBy: '1',
+});
