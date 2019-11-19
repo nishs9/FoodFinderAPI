1360e5a5ed6e7417c3f7c901d2868d3aa035d615
Shashank <shashanknx@gmail.com>
2019-11-18 22:55:18 -0500

    Update ImageController.js

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -6,9 +6,10 @@ exports.postImage = (req, res) => {
   newImage.data = fs.readFileSync(req.body.filepath);
   var imgType = req.body.filepath.substring(req.body.filepath.length - 3, req.body.filepath.length);
   newImage.contentType = 'image/'.concat(imgType);
+  res.send("hello, world");
   newImage.save((err, img) => {
     if (err) {
-      res.status(500).send(err);
+      res.status(501).send(err);
     }
     res.status(201).json({ message: "Image posted!", imgId: img._id });
   });
